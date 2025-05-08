66600fa8f02af1bd89459c92dd0ce79013344591
akel <alevenegaslagos@gmail.com>
2025-05-08 12:35:23 -0400

    Conectado a base de datos MySQL

---
+++ src/main/java/com/educ/EduTech/controller/user.java
@@ -0,0 +1,16 @@
+package com.educ.EduTech.controller;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "usuario")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class user {
+}
